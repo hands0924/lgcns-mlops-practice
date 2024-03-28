4b2787048da79a9787db671e245c69a8eeeb887e
Jay Han <otzslayer@gmail.com>
2024-03-28 02:28:11 +0000

    Apply batch prediction

--- batch_prediction.sh
+++ batch_prediction.sh
@@ -1,4 +1,5 @@
 BASH_ENV=~/.bashrc
-ROOT_PATH=/workspaces/lgcns-mlops-practice
+ROOT_PATH=/workspaces/lgcns-mlops-template
 
 $ROOT_PATH/.venv/bin/python $ROOT_PATH/batch_prediction.py >> $ROOT_PATH/cron.log 2>&1
+Thu Mar 28 02:22:45 UTC 2024
